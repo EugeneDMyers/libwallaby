ec755978de4ad9297890d43e2870ec46cb586f3a
twells46 <173561638+twells46@users.noreply.github.com>
2025-10-10 07:48:36 -0500

    Remove ninja build testing code

--- container/build.sh
+++ container/build.sh
@@ -9,8 +9,6 @@ cd /work/src
 pwd
 ls -alh /opt
 ls -alh /opt/sysroot
-#cmake -Bbuild -G Ninja -DCMAKE_TOOLCHAIN_FILE=./toolchain/aarch64-linux-gnu.cmake -DCMAKE_SYSROOT=/opt/sysroot/ -Dcreate3_build_local=OFF .
-#cmake --build build -j8
 cmake -Bbuild -DCMAKE_TOOLCHAIN_FILE=./toolchain/aarch64-linux-gnu.cmake -DCMAKE_SYSROOT=/opt/sysroot/ -Dcreate3_build_local=OFF .
 cd build
 make -j22 | tee build.log
